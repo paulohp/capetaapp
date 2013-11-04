114e2a1cbbb4f7edfdd753376eb3217e26ec96b3
Paulo Pires <taralloqueiroiz@gmail.com>
2013-11-04 21:58:48 -0200

    mock quase la

--- public/app/scripts/controllers/user.js
+++ public/app/scripts/controllers/user.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('appApp')
-  .controller('UserCtrl', function ($scope) {
+  .controller('UserCtrl', function ($scope, $location) {
   	$scope.user = {};
     $scope.save = function(user){
     	$scope.user.name = user.name;
     	$scope.user.city = user.city;
 
-    	console.log($scope.user);
+    	if ($scope.user.name && $scope.user.city) {
+    		$location.path('/chat/'+$scope.user.city);
+    	};
     }
   });
